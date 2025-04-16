bd495749b15667886134f3d1f4bd2328a0feda05
Yusuke Shinyama <yusuke@bekindlabs.com>
2025-04-16 17:50:21 +0900

    Added: test templates.

--- backend/src/test/kotlin/com/example/todo/TodoApplicationTests.kt
+++ backend/src/test/kotlin/com/example/todo/TodoApplicationTests.kt
@@ -2,7 +2,6 @@ package com.example.todo
 
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.web.servlet.MockMvc
@@ -31,5 +30,41 @@ class TodoApplicationTests {
 			.andExpect(jsonPath("$").isEmpty)
 			.andExpect(jsonPath("$").isArray)
 	}
+
+//	@Test
+//	fun `todoエンドポイントにJSONをPOSTすると、200 OKが返る。`() {
+//	}
+
+//	@Test
+//	fun `todoエンドポイントにJSONをPOSTすると、データベースに追加されている。`() {
+//	}
+
+//	@Test
+//	fun `複数回JSONをPOSTすると、その数だけデータベースに追加されている。`() {
+//	}
+
+//	@Test
+//	fun `GETすると、現在までにPOSTした内容すべてがリストとして返される。`() {
+//	}
+
+//	@Test
+//	fun `POSTすると、新しく追加されたidを返す。`() {
+//	}
+
+//	@Test
+//	fun `存在しないIDに対してGETしようとすると、404エラーを返す。`() {
+//	}
+
+//	@Test
+//	fun `特定のIDに対してGETすると、その項目だけを返す`() {
+//	}
+
+//	@Test
+//	fun `存在しないIDに対してDELETEしようとすると、404エラーを返す。`() {
+//	}
+
+//	@Test
+//	fun `特定のIDに対してDELETEすると、その項目はデータベースから削除される。`() {
+//	}
 
 }
