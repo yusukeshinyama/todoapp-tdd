3d1ae603790052700b9b5e78c704ce49dadf4390
Yusuke Shinyama <yusuke@bekindlabs.com>
2025-06-02 13:45:27 +0900

    Code cleanup.

--- backend/src/main/kotlin/com/example/todo/DynamoDbConfiguration.kt
+++ backend/src/main/kotlin/com/example/todo/DynamoDbConfiguration.kt
@@ -11,20 +11,24 @@ import java.net.URI
 
 @Configuration
 class DynamoDbConfiguration(
-    @Value("\${aws.dynamodb.region}")
+    @Value("\${aws.dynamodb.region:ap-northeast-1}")
     private val region: String,
-    @Value("\${aws.dynamodb.endpoint}")
+    @Value("\${aws.dynamodb.endpoint:http://localhost:4566/}")
     private val endpoint: String,
+    @Value("\${aws.dynamodb.accessKey:xxx}")
+    private val accessKey: String,
+    @Value("\${aws.dynamodb.secretKey:yyy}")
+    private val secretKey: String,
 ) {
     private val logger = LoggerFactory.getLogger(javaClass)
 
     @Bean
-    fun dummyCredentials(): AwsCredentials {
-        return AwsBasicCredentials.create("xxx", "yyy")
+    fun awsCredentials(): AwsBasicCredentials {
+        return AwsBasicCredentials.create(accessKey, secretKey)
     }
 
     @Bean
-    fun credentialsProvider(credentials: AwsCredentials): AwsCredentialsProviderChain {
+    fun credentialsProvider(credentials: AwsBasicCredentials): AwsCredentialsProviderChain {
         return AwsCredentialsProviderChain.of(
             DefaultCredentialsProvider.create(),
             StaticCredentialsProvider.create(credentials),
