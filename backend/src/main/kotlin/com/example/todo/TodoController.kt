a7037d56bdca329802c226b54b34db00d3fb9591
Yusuke Shinyama <yusuke@bekindlabs.com>
2025-06-20 15:39:04 +0900

    Added: implementation.

--- backend/src/main/kotlin/com/example/todo/TodoController.kt
+++ backend/src/main/kotlin/com/example/todo/TodoController.kt
@@ -1,9 +1,98 @@
 package com.example.todo
 
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient
+import software.amazon.awssdk.services.dynamodb.model.AttributeValue.fromS
+import software.amazon.awssdk.services.dynamodb.model.DeleteItemRequest
+import software.amazon.awssdk.services.dynamodb.model.GetItemRequest
+import software.amazon.awssdk.services.dynamodb.model.PutItemRequest
+import software.amazon.awssdk.services.dynamodb.model.ScanRequest
+import java.util.UUID
 
 @RestController
 class TodoController(val dynamoDbClient: DynamoDbClient) {
+
+    @GetMapping("/api/todo")
+    fun getTodos(): List<TodoEntity> {
+        val scanRequest = ScanRequest
+            .builder()
+            .tableName("todo")
+            .build()
+        val scanResponse = dynamoDbClient.scan(scanRequest)
+        val items = scanResponse.items().toList()
+        return items.map {
+            TodoEntity(id = it["PK"]!!.s(), text = it["text"]!!.s())
+        }
+    }
+
+    @GetMapping("/api/todo/{id}")
+    fun getTodo1(@PathVariable id: String): ResponseEntity<TodoEntity> {
+        val key1 = mapOf(
+            "PK" to fromS(id),
+        )
+        val getItemRequest = GetItemRequest
+            .builder()
+            .tableName("todo")
+            .key(key1)
+            .build()
+        val getItemResponse = dynamoDbClient.getItem(getItemRequest)
+        if (getItemResponse.hasItem()) {
+            val item = getItemResponse.item()
+            return ResponseEntity(TodoEntity(id = item["PK"]!!.s(), text = item["text"]!!.s()), HttpStatus.OK)
+        }
+        return ResponseEntity(HttpStatus.NOT_FOUND)
+    }
+
+    @PostMapping("/api/todo")
+    fun postTodo(@RequestBody req: TodoRequest): String {
+        // テーブルに値を追加。
+        val id = UUID.randomUUID().toString()
+        val item1 = mapOf(
+            "PK" to fromS(id),
+            "text" to fromS(req.text),
+        )
+        val putItemRequest = PutItemRequest
+            .builder()
+            .tableName("todo")
+            .item(item1)
+            .build()
+        dynamoDbClient.putItem(putItemRequest)
+        return id
+    }
+
+    @DeleteMapping("/api/todo/{id}")
+    fun deleteTodo(@PathVariable id: String): ResponseEntity<Void> {
+        // テーブルから値を取得。
+        val key1 = mapOf(
+            "PK" to fromS(id),
+        )
+        val getItemRequest = GetItemRequest
+            .builder()
+            .tableName("todo")
+            .key(key1)
+            .build()
+        val getItemResponse = dynamoDbClient.getItem(getItemRequest)
+        if (!getItemResponse.hasItem()) {
+            return ResponseEntity(HttpStatus.NOT_FOUND)
+        }
+        // テーブルから値を削除。
+        val key2 = mapOf(
+            "PK" to fromS(id),
+        )
+        val deleteItemRequest = DeleteItemRequest
+            .builder()
+            .tableName("todo")
+            .key(key2)
+            .build()
+        dynamoDbClient.deleteItem(deleteItemRequest)
+        return ResponseEntity(HttpStatus.OK)
+    }
 
 }
--- backend/src/test/kotlin/com/example/todo/TodoApplicationTests.kt
+++ backend/src/test/kotlin/com/example/todo/TodoApplicationTests.kt
@@ -1,11 +1,22 @@
 package com.example.todo
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.equalTo
+import org.hamcrest.Matchers.hasItems
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient
 import software.amazon.awssdk.services.dynamodb.model.AttributeValue.fromS
 import software.amazon.awssdk.services.dynamodb.model.DeleteItemRequest
@@ -27,14 +38,60 @@ class TodoApplicationTests {
 	@Autowired
 	private lateinit var dynamoDbClient: DynamoDbClient
 
+	val tableName = "todo"
+
 	@Test
 	fun contextLoads() {
+	}
+
+	@BeforeEach
+	fun setUp() {
+		// テーブルを空にする。
+		val scanRequest = ScanRequest
+			.builder()
+			.tableName("todo")
+			.build()
+		val scanResponse = dynamoDbClient.scan(scanRequest)
+		for (item in scanResponse.items()) {
+			val key = mapOf(
+				"PK" to item["PK"],
+			)
+			val deleteItemRequest = DeleteItemRequest
+				.builder()
+				.tableName("todo")
+				.key(key)
+				.build()
+			dynamoDbClient.deleteItem(deleteItemRequest)
+		}
+	}
+
+	// todoをポストし、IDを返す。
+	private fun postTodo(data: TodoRequest): String {
+		val result = mockMvc
+			.perform(
+				post("/api/todo")
+					.contentType(MediaType.APPLICATION_JSON)
+					.content(objectMapper.writeValueAsString(data))
+			)
+			.andReturn()
+		return result.response.contentAsString
+	}
+
+	// データベースからtodoの一覧を取得する。
+	private fun getTodos(): List<TodoEntity> {
+		val scanRequest = ScanRequest
+			.builder()
+			.tableName(tableName)
+			.build()
+		val scanResponse = dynamoDbClient.scan(scanRequest)
+		return scanResponse.items().toList().map {
+			TodoEntity(id=it["PK"]!!.s(), text=it["text"]!!.s())
+		}
 	}
 
 	@Test
 	fun dynamoDbTest() {
 		// DynamoDB関連の機能テスト。
-		val tableName = "todo"
 		val id = UUID.randomUUID().toString()
 
 		// テーブルに値を追加。
@@ -85,59 +142,193 @@ class TodoApplicationTests {
 		dynamoDbClient.deleteItem(deleteItemRequest)
 	}
 
-//	@Test
-//	fun `初期状態でtodoエンドポイントをGETすると、空のリストが返る。`() {
-//		mockMvc
-//			.perform(
-//				get("/api/todo")
-//			)
-//			.andExpect(status().isOk)
-//			.andExpect(jsonPath("$").isEmpty)
-//			.andExpect(jsonPath("$").isArray)
-//	}
+	@Test
+	fun `初期状態でtodoエンドポイントをGETすると、空のリストが返る。`() {
+		mockMvc
+			.perform(
+				get("/api/todo")
+			)
+			.andExpect(status().isOk)
+			.andExpect(jsonPath("$").isEmpty)
+			.andExpect(jsonPath("$").isArray)
+	}
 
-//	@Test
-//	fun `todoエンドポイントにJSONをPOSTすると、200 OKが返る。`() {
-//		val data = TodoRequest(text="abc")
-//		mockMvc
-//			.perform(
-//				post("/api/todo")
-//					.contentType(MediaType.APPLICATION_JSON)
-//					.content(objectMapper.writeValueAsString(data))
-//			)
-//			.andExpect(status().isOk)
-//	}
+	@Test
+	fun `todoエンドポイントにJSONをPOSTすると、200 OKが返る。`() {
+		val data = TodoRequest(text="abc")
+		mockMvc
+			.perform(
+				post("/api/todo")
+					.contentType(MediaType.APPLICATION_JSON)
+					.content(objectMapper.writeValueAsString(data))
+			)
+			.andExpect(status().isOk)
+	}
 
-//	@Test
-//	fun `todoエンドポイントにJSONをPOSTすると、データベースに追加されている。`() {
-//	}
+	@Test
+	fun `todoエンドポイントにJSONをPOSTすると、データベースに追加されている。`() {
+		// Arrange
 
-//	@Test
-//	fun `複数回JSONをPOSTすると、その数だけデータベースに追加されている。`() {
-//	}
+		// Act
+		// {text:"abc"}をポストする
+		val data = TodoRequest(text = "abc")
+		val id = postTodo(data)
 
-//	@Test
-//	fun `GETすると、現在までにPOSTした内容すべてがリストとして返される。`() {
-//	}
+		// Assert
+		val items = getTodos()
+		// テーブルを全取得すると長さ1。
+		assertThat(items.size, equalTo(1))
+		// 0番目の要素が "abc" である。
+		assertThat(items[0].text, equalTo(data.text))
+	}
 
-//	@Test
-//	fun `POSTすると、新しく追加されたidを返す。`() {
-//	}
+	@Test
+	fun `複数回JSONをPOSTすると、その数だけデータベースに追加されている。`() {
+		// Arrange
 
-//	@Test
-//	fun `存在しないIDに対してGETしようとすると、404エラーを返す。`() {
-//	}
+		// Act
+		// {text:"abc"}をポストする
+		val data1 = TodoRequest(text="abc")
+		postTodo(data1)
+		// {text:"def"}をポストする
+		val data2 = TodoRequest(text="def")
+		postTodo(data2)
 
-//	@Test
-//	fun `特定のIDに対してGETすると、その項目だけを返す`() {
-//	}
+		// Assert
+		val items = getTodos()
+		// テーブルを全取得すると長さ2。
+		assertThat(items.size, equalTo(2))
+		val texts = items.map { it.text }
+		// 2つの要素が "abc", "def" である。
+		assertThat(texts, hasItems(data1.text, data2.text))
+	}
 
-//	@Test
-//	fun `存在しないIDに対してDELETEしようとすると、404エラーを返す。`() {
-//	}
+	@Test
+	fun `GETすると、現在までにPOSTした内容すべてがリストとして返される。`() {
+		// Arrange
+		// {text:"abc"}をポストする
+		val data1 = TodoRequest(text="abc")
+		postTodo(data1)
+		// {text:"def"}をポストする
+		val data2 = TodoRequest(text="def")
+		postTodo(data2)
 
-//	@Test
-//	fun `特定のIDに対してDELETEすると、その項目はデータベースから削除される。`() {
-//	}
+		// Act
+		// /api/todoをGETする。
+		val result = mockMvc.perform(
+			get("/api/todo")
+		)
+			.andReturn()
+			.response.contentAsString
+		val items = objectMapper.readValue<List<TodoRequest>>(result)
+
+		// Assert
+		// 2つの要素が "abc", "def" である。
+		assertThat(items.size, equalTo(2))
+		val texts = items.map { it.text }
+		assertThat(texts, hasItems(data1.text, data2.text))
+	}
+
+	@Test
+	fun `POSTすると、新しく追加されたidを返す。`() {
+		// Act
+		// {text:"abc"}をポストし、idを取得する。
+		val data1 = TodoRequest(text="abc")
+		val id = postTodo(data1)
+
+		// Assert
+		// 返されたidの項目がテーブル中に存在している。
+		val key1 = mapOf(
+			"PK" to fromS(id),
+		)
+		val getItemRequest = GetItemRequest
+			.builder()
+			.tableName(tableName)
+			.key(key1)
+			.build()
+		val item2 = dynamoDbClient.getItem(getItemRequest).item()
+		assertThat(item2["PK"]!!.s(), equalTo(id))
+		assertThat(item2["text"]!!.s(), equalTo(data1.text))
+	}
+
+	@Test
+	fun `存在しないIDに対してGETしようとすると、404エラーを返す。`() {
+		// Act
+		// id=999 をGETする。
+		mockMvc
+			.perform(
+				get("/api/todo/999")
+			)
+			// Assert
+			// 404が返される。
+			.andExpect(status().isNotFound)
+	}
+
+	@Test
+	fun `特定のIDに対してGETすると、その項目だけを返す`() {
+		// Arrange
+		// {text:"abc"}をポストし、idを取得する。
+		val data1 = TodoRequest(text="abc")
+		val id = postTodo(data1)
+
+		// act
+		// /api/todo/id をGETする。
+		mockMvc
+			.perform(
+				get("/api/todo/$id")
+			)
+			// Assert
+			// 200が返される。
+			.andExpect(status().isOk)
+			// idが一致。
+			.andExpect(jsonPath("$.id").value(id))
+			// textが一致。
+			.andExpect(jsonPath("$.text").value(data1.text))
+	}
+
+	@Test
+	fun `存在しないIDに対してDELETEしようとすると、404エラーを返す。`() {
+		// Act
+		// id=999 をDELETEする。
+		mockMvc
+			.perform(
+				delete("/api/todo/999")
+			)
+			// Assert
+			// 404が返される。
+			.andExpect(status().isNotFound)
+	}
+
+	@Test
+	fun `特定のIDに対してDELETEすると、その項目はデータベースから削除される。`() {
+		// Arrange
+		// {text:"abc"}をポストし、idを取得する。
+		val data1 = TodoRequest(text="abc")
+		val id = postTodo(data1)
+
+		// act
+		// /api/todo/id をDELETEする。
+		mockMvc
+			.perform(
+				delete("/api/todo/$id")
+			)
+			// Assert
+			// 200が返される。
+			.andExpect(status().isOk)
+
+		// Assert
+		// 返されたidの項目がテーブル中に存在していない。
+		val key1 = mapOf(
+			"PK" to fromS(id),
+		)
+		val getItemRequest = GetItemRequest
+			.builder()
+			.tableName(tableName)
+			.key(key1)
+			.build()
+		val getItemResponse = dynamoDbClient.getItem(getItemRequest)
+
+		assertThat(getItemResponse.hasItem(), equalTo(false))
+	}
 
 }
